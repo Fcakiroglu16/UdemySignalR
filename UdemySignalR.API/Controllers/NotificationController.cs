f9556453a3f4bd6d48550819c31b7688027e67a5
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2020-05-13 11:45:20 +0300

    SignalR Server IHubContext<>

---
+++ UdemySignalR.API/Controllers/NotificationController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using UdemySignalR.API.Hubs;
+
+namespace UdemySignalR.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationController : ControllerBase
+    {
+        private readonly IHubContext<MyHub> _hubContext;
+
+        public NotificationController(IHubContext<MyHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        [HttpGet("{teamCount}")]
+        public async Task<IActionResult> SetTeamCount(int teamCount)
+        {
+            await _hubContext.Clients.All.SendAsync("Notify", $"Arkadaşlar takım  {teamCount} kişi olacaktır.");
+
+            return Ok();
+        }
+    }
+}
