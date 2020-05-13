6dc5a0575ecb30202f90eeed9c9791b03dfea13d
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2020-05-13 11:04:31 +0300

    SignalR Server Hub virtual methods

--- UdemySignalR.API/Hubs/MyHub.cs
+++ UdemySignalR.API/Hubs/MyHub.cs
@@ -8,7 +8,9 @@ namespace UdemySignalR.API.Hubs
 {
     public class MyHub : Hub
     {
-        public static List<string> Names { get; set; } = new List<string>();
+        private static List<string> Names { get; set; } = new List<string>();
+
+        private static int ClientCount { get; set; } = 0;
 
         public async Task SendName(string name)
         {
@@ -20,6 +22,22 @@ namespace UdemySignalR.API.Hubs
         public async Task GetNames()
         {
             await Clients.All.SendAsync("ReceiveNames", Names);
+        }
+
+        public async override Task OnConnectedAsync()
+        {
+            ClientCount++;
+            await Clients.All.SendAsync("ReceiveClientCount", ClientCount);
+
+            await base.OnConnectedAsync();
+        }
+
+        public async override Task OnDisconnectedAsync(Exception exception)
+        {
+            ClientCount--;
+            await Clients.All.SendAsync("ReceiveClientCount", ClientCount);
+
+            await base.OnDisconnectedAsync(exception);
         }
     }
 }
