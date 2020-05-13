0e5903012f77afd200c3e51a56f0baf2a5434ba9
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2020-05-13 16:28:12 +0300

    SignalR Server Strongly typed Hubs

---
+++ UdemySignalR.API/Hubs/IProductHub.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UdemySignalR.API.Models;
+
+namespace UdemySignalR.API.Hubs
+{
+    public interface IProductHub
+    {
+        Task ReceiveProduct(Product p);
+    }
+}
---
+++ UdemySignalR.API/Hubs/ProductHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UdemySignalR.API.Models;
+
+namespace UdemySignalR.API.Hubs
+{
+    public class ProductHub : Hub<IProductHub>
+    {
+        public async Task SendProduct(Product p)
+        {
+            await Clients.All.ReceiveProduct(p);
+        }
+    }
+}
